44eece5b29b6ddf4eaeaa428b42f5f750b375b96
allantucker <60186352+allantucker@users.noreply.github.com>
2020-01-27 15:40:30 -0500

    Updated connection details

--- config.php
+++ config.php
@@ -5,7 +5,7 @@
     define('SITE_URL','http://localhost/btm/awstreams/');
 
     // database config
-    define('DB_SERVER', 'localhost');
+    define('DB_SERVER', '201.87.65.109');
     define('DB_USERNAME', 'root');
     define('DB_PASSWORD', 'kja876dsf76FDFfdsF8524UYUYGygug');
     define('DB_NAME', 'homedbftw');
